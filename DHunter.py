c1cb3a4356e7f3e65e80057e0d7089a63c4780c1
Indian Cyber Force <148000370+BharatCyberForce@users.noreply.github.com>
2025-04-21 22:15:59 +0530

    Update DHunter.py

--- DHunter.py
+++ DHunter.py
@@ -19,7 +19,7 @@ def captcha(driver):
         iframe = driver.find_element(By.XPATH, "//iframe[@title='reCAPTCHA']")
         driver.switch_to.frame(iframe)
 
-        checkbox = WebDriverWait(driver, 6).until(
+        checkbox = WebDriverWait(driver, 10).until(
             EC.element_to_be_clickable((By.XPATH, "//div[@class='recaptcha-checkbox-checkmark']"))
         )
         checkbox.click()
@@ -48,12 +48,12 @@ def scraper(dork, op):
         #options.add_argument(r"/home/evil_linux/.mozilla/firefox/3to5its4.default-esr")
         #driver = webdriver.Firefox(service=Service(), options=options)
         driver.get(f"https://www.google.com/")
-        WebDriverWait(driver,2).until(EC.presence_of_element_located((By.NAME, "q")))
+        WebDriverWait(driver, 2).until(EC.presence_of_element_located((By.NAME, "q")))
 
         search_box = driver.find_element(By.NAME, "q")
         search_box.send_keys(dork)
         search_box.send_keys(Keys.RETURN)
-        time.sleep(1)
+        time.sleep(2)
 
         with open(op, "w") as file:
             #for page in range(1, numb + 1):
@@ -64,7 +64,7 @@ def scraper(dork, op):
                 if captcha(driver):
                     print(Fore.LIGHTRED_EX+"Uff! CaptchA Detected.")
 
-                    input(Fore.GREEN+"Press Enter After Solve Captcha...")
+                    input(Fore.GREEN+"Press Enter After Solving The Captcha...")
                     time.sleep(2)
 
                 links = driver.find_elements(By.XPATH, "//a[contains(@href, 'http')]")
@@ -81,7 +81,7 @@ def scraper(dork, op):
                     try:
                         more=driver.find_element(By.XPATH,"//div[contains(@class, 'TOQyFc')]")#//span[contains(@class, 'PBBEhf') and text()='More search results']")))
                         more.click()
-                        time.sleep(random.uniform(1, 3))
+                        time.sleep(random.uniform(2, 4))
                     except:
                         print(Fore.LIGHTRED_EX+"Ops! No More Pages Not Found!.")
                         break
@@ -94,7 +94,7 @@ def scraper(dork, op):
 def banner():
     base=Fore.LIGHTMAGENTA_EX+"""
     ========================================
-    |            Dork Hunter Classic       |
+    |          Dork Hunter Classic         |
     |    Developed by: Indian Cyber Force  |
     ========================================\n\n"""
     print(base)
